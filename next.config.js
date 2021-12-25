f21250c3965fa8161fd81b60ec0c6b4bb6204e2d
Morteza Sabihi <mortezasabihi87@gmail.com>
2021-12-25 19:52:50 +0330

    fix: social media icons replaced

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };

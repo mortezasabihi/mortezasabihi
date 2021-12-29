197abaadc8da7a52a7fa62626ab9fa422b6b5f4b
Morteza Sabihi <mortezasabihi87@gmail.com>
2021-12-29 12:57:10 +0330

    feat: skills item animation updated

--- components/Page/Resume/Skills/Item.tsx
+++ components/Page/Resume/Skills/Item.tsx
@@ -8,14 +8,25 @@ type Props = {
 };
 
 const Item = ({ label, color, children }: Props) => {
+  const hexToRGBA = (hex: string, alpha: string) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  };
+
   return (
     <motion.div
-      whileHover={{ scale: 1.3 }}
+      whileHover={{
+        scale: 1.3,
+        borderRadius: ["0%", "20%", "50%", "50%"],
+      }}
       transition={{ type: "spring", stiffness: 300 }}
-      className={`w-20 h-20 flex flex-col items-center justify-center mx-auto ${
+      className={`w-20 h-20 flex flex-col items-center justify-center mx-auto cursor-default ${
         color === "#000000" && "text-white"
       }`}
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: hexToRGBA(color, "0.8") }}
     >
       <div className="w-8 h-8">{children}</div>
 

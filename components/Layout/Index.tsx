f68b07f041ff9af5fdc0c2ae569e1487580d760f
Morteza Sabihi <mortezasabihi87@gmail.com>
2021-12-29 14:04:16 +0330

    feat: download resume button added

--- components/Layout/Index.tsx
+++ components/Layout/Index.tsx
@@ -19,6 +19,18 @@ const Layout = ({ children }: Props) => {
         {children}
       </motion.main>
 
+      <motion.a
+        download
+        href="/Resume.pdf"
+        target="_blank"
+        className="
+        text-white fixed bottom-0 right-0 mr-14 mb-14 px-5 py-2 text-lg tracking-wider border-2 border-white rounded-full hidden md:block
+      "
+        whileHover={{ scale: 1.3, backgroundColor: "#fff", color: "#4f46e5" }}
+      >
+        Get My Resume
+      </motion.a>
+
       <footer>
         <SocialMedia />
       </footer>

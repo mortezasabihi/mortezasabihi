b5d6d7dcdee08e83f9968b8ea8d428f6e9cab5b0
Morteza Sabihi <mortezasabihi87@gmail.com>
2022-01-12 14:22:28 +0330

    feat: google analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { motion } from "framer-motion";
 import { Layout } from "components";
 import "../styles/globals.css";
@@ -16,16 +17,32 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   };
 
   return (
-    <Layout>
-      <motion.div
-        key={router.route}
-        initial="initial"
-        animate="animate"
-        variants={variants}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </Layout>
+    <>
+      <Script
+        strategy={"lazyOnload"}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="ga-script" strategy={"lazyOnload"}>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
+      <Layout>
+        <motion.div
+          key={router.route}
+          initial="initial"
+          animate="animate"
+          variants={variants}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </Layout>
+    </>
   );
 }
 

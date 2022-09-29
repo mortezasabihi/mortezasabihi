ba55d82607ee8eb6a5f496b2138520f016a1d3ad
Morteza Sabihi <mortezasabihi87@gmail.com>
2022-09-29 14:26:58 +0330

    chore: update about text

--- components/Shared/AboutText.tsx
+++ components/Shared/AboutText.tsx
@@ -1,11 +1,11 @@
 const AboutText = () => {
   return (
     <p className="font-light leading-relaxed text-gray-900 mb-8">
-      Experienced as a front-end developer with <strong>+3</strong> years of
-      building web applications, Using Javascript frameworks likes: VueJS,
-      NuxtJS, ReactJS, NextJS paired with Gitflow, and third party libraries to
-      build well-performing, great UIs for stability. Enjoying working in teams
-      in Agile-driven environments to manage projects and tasks.
+      Experienced as a front-end developer with 4+ years of building web
+      applications, Using Javascript frameworks likes: VueJS, NuxtJS, ReactJS,
+      NextJS paired with Gitflow, and third party libraries to build
+      well-performing, great UIs for stability. Enjoying working in teams in
+      Agile-driven environments to manage projects and tasks.
     </p>
   );
 };

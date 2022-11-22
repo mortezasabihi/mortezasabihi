44be95a567b7e5f376b44bb8e7484eaa5989babe
Morteza Sabihi <35567646+mortezasabihi@users.noreply.github.com>
2022-11-22 16:13:42 +0330

    chore: update experiences

--- components/Page/Resume/Experiences/index.tsx
+++ components/Page/Resume/Experiences/index.tsx
@@ -19,28 +19,23 @@ const Experiences = () => {
       date: "DEC 2019 - JUL 2020",
       type: "Part Time",
       description: [
-        "Manage website development project from initial design through completion using VueJS and NuxtJS frameworks, optimizing all cross-browser and multi-platform compatibility, and leading the project alone in the first 6 months.",
+        "Managed website development project from initial design through completion using Vue.js and Nuxt.js frameworks, optimizing all cross-browser and multi-platform compatibility, and leading tasks alone in the first 6 months",
         "Designed and prepared end-to-end product features in collaboration with UI/UX designers and stakeholders that enhanced performance by up to 80%.",
         "Made website pages SEO Friendly by developing in SSR mode, adding required meta tags, applying JSON-LD, reducing image size, and lazy-loading.",
+        "Used SCSS pre-processor for styling the project with BEM naming methodology and structuring files with 7-1 pattern.",
+        "Added multi-language support with i18n library."
       ],
     },
     {
-      company: "7even Tech Team",
-      date: "DEC 2019 - MAY 2020",
+      company: "7even Tech",
+      date: "SEP 2020 - MAY 2022",
       type: "Full Time",
       description: [
-        "Worked closely with international programmers, UI/UX designers, and clients to meet requirements, goals, and desired functionality on a challenging and complex Startup project.",
-        "Developed the web application from initial UI design using VueJS and NuxtJS frameworks with members of a team of 4 people.",
+        "Collaborated closely with international programmers teams, UI/UX designers, and clients to meet requirements, goals, and suitable functionality on a challenging and complex Startup project.",
+        "Developed the web application from initial UI design using Vue.js and NuxtJS frameworks with members of a team of 4 people.",
+        "Translating UI designs to maintainable code with clean code architecture and connecting to backend service using Rest API.",
         "Took charge of developing MVP as a team member and working overtime to ensure that it got completed by the deadline.",
-      ],
-    },
-    {
-      company: "Freelance",
-      date: "JULY 2021 - MAY 2022",
-      type: "Full Time",
-      description: [
-        "Worked closely with clients to meet requirements, goals, and desired functionality.",
-        "Contributed as an Open-Source developer on github.",
+        "Updated the project Vue.js version from 2 to 3 along with typescript and used composable functions to create reusable code."
       ],
     },
     {
@@ -48,10 +43,12 @@ const Experiences = () => {
       date: "JUN 2022 - SEP 2022",
       type: "Full Time",
       description: [
-        "Worked closely with UI/UX designers and ask them to design every page with different statuses and a complete design system. Coded the entire design system and pages with tailwindcss, next.js, and typescript.",
-        "Communicated with the team to meet the client's requirements, goals, and desired functionality. Had a meeting with the Manager to solve the project issues.",
+        "Translated UI designs to actual code by working closely with UI/UX designers and asking them to design every page with different statuses and a complete design system. Coded the entire design system and pages with TailwindCSS, Next.js, and Typescript.",
+        "Delivered at least 2 React.js Web UI projects with cross-browser compatible pages using the latest web technologies.",
+        "Communicated with the team to meet the client's requirements, goals, and desired functionality. Had meetings with the Manager to solve the project issues.",
         "Had a chance to manage a project and create tickets for the project.",
-        "Coded Nextjs, typescript web application with clean structure. Created a Nextjs boilerplate for the company.",
+        "Coded Next.js typescript web application with a clean structure. Architected a scalable, reliable, and reusable Next.js boilerplate for the company to speed up development for future projects.",
+        "Used HLS protocol to build a video streaming web application with full support for all kinds of devices.",
       ],
     },
   ];
